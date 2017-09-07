bcbd48ed7555e75c4541892c2d4be17dd43268cc
IgorZaplavskiy <i.zaplavskiy@gmail.com>
2017-09-07 18:27:34 +0300

    Update photo zoom

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+
+	// zoom_photo
+
+	$(".header_left a").append("<span></span>");
+	$(".header_left a").hover(function() {
+		
+		$(this).children("span").fadeIn(600);
+		}, function() {
+		$(this).children("span").fadeOut(200);
+
+		
+	});
+		
+});
